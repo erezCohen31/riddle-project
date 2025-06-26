197866e8e0aa284edf61eb03d7d830317fd9cfab
erez cohen <erezjbh@gmail.com>
2025-06-26 15:44:36 +0200

    add fn main to app.js

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ import MultipleChoiceRiddle from "./classes/MultipliChoiceRiddle.js"
 import riddles from "./classes/Import.js";
 import readline from 'readline-sync';
 
+function main() {
+    const name = readline.question("enter your name:\n")
+    console.log("Hello " + name);
 
-const name = readline.question("enter your name:\n")
-console.log("Hello " + name);
+    const player = new Player(name)
 
-const player = new Player(name)
+    for (const riddleData of riddles) {
+        const riddle = new MultipleChoiceRiddle(riddleData)
+        const time = riddle.startQuestion()
+        console.log(` You took ${time} ms to answer.`);
 
-for (const riddleData of riddles) {
-    const riddle = new MultipleChoiceRiddle(riddleData)
-    const time = riddle.startQuestion()
-    console.log(` You took ${time} ms to answer.`);
-
-    player.addTime(time)
+        player.addTime(time)
+    }
+    player.showStats()
 }
-player.showStats()
+
+main()
+
