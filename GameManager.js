aa834bb7661a74cab55df1d4ee73eba4dedbfb53
erez cohen <erezjbh@gmail.com>
2025-07-01 11:39:44 +0300

    add try/catch

--- GameManager.js
+++ GameManager.js
@@ -4,17 +4,21 @@ import riddles from "./classes/Import.js";
 import readline from 'readline-sync';
 
 export default function main() {
-    const name = readline.question("enter your name:\n")
-    console.log("Hello " + name);
+    try {
+        const name = readline.question("enter your name:\n")
+        console.log("Hello " + name);
 
-    const player = new Player(name)
+        const player = new Player(name)
 
-    for (const riddleData of riddles) {
-        const riddle = new MultipleChoiceRiddle(riddleData)
-        const time = riddle.startQuestion()
-        console.log(` You took ${time} ms to answer.`);
+        for (const riddleData of riddles) {
+            const riddle = new MultipleChoiceRiddle(riddleData)
+            const time = riddle.startQuestion()
+            console.log(` You took ${time} ms to answer.`);
 
-        player.addTime(time)
+            player.addTime(time)
+        }
+        player.showStats()
+    } catch (err) {
+        console.error("An unexpected error occurred:", err.message);
     }
-    player.showStats()
 }
