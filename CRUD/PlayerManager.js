2369e65a035fef38dcd7fac39cc10ef0460e2026
erez cohen <erezjbh@gmail.com>
2025-07-03 22:22:52 +0300

    show if updta a better time

--- CRUD/PlayerManager.js
+++ CRUD/PlayerManager.js
@@ -8,14 +8,14 @@ export async function findOrCreatePlayer(filePath, name) {
         let player = players.find(player => player.name === name);
 
         if (!player) {
-            player = new Player(name, players.length + 1)
+            player = new Player(name, players.length + 1, 0)
 
             players.push(player);
 
             await write(filePath, players);
             console.log("New player added!");
         } else {
-            player = new Player(name, player.id)
+            player = new Player(name, player.id, player.lowestTime)
         }
 
         return player;
@@ -32,9 +32,11 @@ export async function UpdateTimeOfPlayer(filePath, id, time) {
     const player = players.find(player => player.id === id);
     if (!player) {
         console.log("player not exist");
+        return false
 
     } else if (player.lowestTime !== 0 && player.lowestTime <= time) {
         console.log(`Existing time is better or equal, no update.`);
+        return false
 
     } else {
 
@@ -44,6 +46,7 @@ export async function UpdateTimeOfPlayer(filePath, id, time) {
             player.lowestTime = time;
             await write(filePath, players);
             console.log(`Time updated for player ${player.name}: ${time}`);
+            return true
         } catch (err) {
             console.error("Error:", err.message);
         }
