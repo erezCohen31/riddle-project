f4d451058ae2e058096e75f041efb89483e53004
erez cohen <erezjbh@gmail.com>
2025-07-03 15:12:03 +0300

    add the function update and create

---
+++ CRUD/Update.js
@@ -0,0 +1,44 @@
+import { read, write } from "./fileHelper.js";
+import readline from 'readline-sync';
+
+
+async function updateRiddle(filePath, targetId) {
+    try {
+        const riddles = await read(filePath);
+        const riddlesChanged = changeRiddle(riddles, targetId);
+        await write(filePath, riddlesChanged);
+        console.log(" riddle changed !");
+    } catch (err) {
+        console.error("error :", err.message);
+    }
+}
+
+function changeRiddle(riddles, targetId) {
+
+    const riddle = riddles.find(riddle => riddle.id === targetId);
+    if (!riddle) {
+        console.log(`No riddle found with id ${targetId}`);
+        return riddles;
+    }
+    const fieldToChange = readline.question("What field do you want to change? (name, taskDescription, correctAnswer, choices): ");
+    if (!(fieldToChange in riddle)) {
+        console.log(`Field "${fieldToChange}" does not exist.`);
+        return riddles;
+    }
+    if (fieldToChange === "choices") {
+        const newChoices = [];
+        for (let i = 0; i < 4; i++) {
+            const choice = readline.question(`Choice ${i + 1}: `);
+            newChoices.push(choice);
+        }
+        riddle[fieldToChange] = newChoices;
+    } else {
+        riddle[fieldToChange] = readline.question("enter the new value:")
+    }
+    return riddles
+}
+
+
+const filePath = '../DB/riddles.txt';
+
+updateRiddle(filePath, 11)
